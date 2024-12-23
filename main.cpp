31ca9a7b8ea0e5e71709b7e4a10f715f48f42e5c
MohammadYehya <mohammad7446@gmail.com>
2024-12-23 18:40:32 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@ int main( int argc, char *argv[] )
 {
     SDL_Init( SDL_INIT_EVERYTHING );
 
-    SDL_Window *window = SDL_CreateWindow( "Hello SDL WORLD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI );
+    SDL_DisplayMode DM;
+    SDL_GetCurrentDisplayMode(0, &DM);
+
+
+    SDL_Window *window = SDL_CreateWindow( "GridForge", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED );
 
     if ( NULL == window )
     {
@@ -15,14 +19,30 @@ int main( int argc, char *argv[] )
         return 1;
     }
 
-    SDL_Event windowEvent;
+    // SDL_Surface* icon = IMG_Load("assets/system/interface/icon.png");
+    // SDL_Surface* icon = SDL_CreateRGBSurfaceFrom();
+    // SDL_SetWindowIcon(window, icon);
+    // SDL_FreeSurface(icon);
 
+    SDL_Event windowEvent;
+    int x = 0 , y = 0;
     while ( true )
     {
         if ( SDL_PollEvent( &windowEvent ) )
         {
             if ( SDL_QUIT == windowEvent.type )
             { break; }
+            else if (SDL_MOUSEBUTTONDOWN == windowEvent.type)
+            {
+                //TESTS
+                //-----------------------------------------------------------------------------------------------------------------------------------
+                // SDL_GetMouseState(&x, &y);
+                // std::cout << SDL_GetCPUCount() << '\n' << SDL_GetSystemRAM() << '\n' << SDL_GetClipboardText() << '\n' << x << '-' << y << '\n';
+
+                // SDL_OpenURL("https://github.com/MohammadYehya");
+
+                // SDL_RenderDrawLine(,100,100,300,300)
+            }
         }
     }
 
