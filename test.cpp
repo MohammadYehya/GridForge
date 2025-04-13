6694f190bda4fd856d0e0ff5fa3bec901fd89ee6
MohammadYehya <mohammad7446@gmail.com>
2025-04-13 12:20:30 +0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -129,7 +129,7 @@ int main(int argc, char* argv[]) {
 
     // Create window
     SDL_Window* window = SDL_CreateWindow(
-        "Isometric Grid with Movable Player", 
+        "GridForge", 
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
         800, 600, 
         SDL_WINDOW_OPENGL
